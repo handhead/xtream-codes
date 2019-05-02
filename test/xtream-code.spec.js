e3bd11f61470222c9cc100bdacf360abdcdcb280
Vinícius Rodrigues <viniciusclerod@gmail.com>
2019-05-02 16:37:57 -0300

    Tests

--- test/xtream-code.spec.js
+++ test/xtream-code.spec.js
@@ -1,14 +1,34 @@
-var procreate = require("../lib")
+var Panel = require("../lib").Panel
 var chai = require("chai"),
     expect = chai.expect;
 
 describe("Xtream Code", function () {
-    describe('say', function () {
-        it('nothing', function () {
-            expect(procreate.say()).to.be.an('undefined');
+    describe('Instance', function () {
+        it('is an function', function () {
+            expect(Panel).to.be.an('function');
         });
-        it('something', function () {
-            expect(procreate.say("hi")).to.be.a('string');
+        it('empty', function () {
+            var empty = new Panel();
+            expect(empty).to.be.an.instanceof(Panel)
+                .and.to.have.property('url', undefined)
+                .and.to.have.property('key', undefined);
+        });
+        it('new', function () {
+            // http://179.124.136.19:8000/api.php?action=user&sub=create
+            // username
+            // password
+            // max_connections
+            // is_restreamer
+            // exp_date
+            // bouquet
+            // api_pass
+
+            var panel = new Panel('http://179.124.136.19:8000', 'handhead@2019');
+            expect(panel).to.be.an.instanceof(Panel)
+                .and.to.have.property('url', 'http://179.124.136.19:8000')
+                .and.to.have.property('key', 'handhead@2019');
         });
     });
+
+
 });
